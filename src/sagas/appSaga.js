4c8360b06492717b05faedc92cf44dd41bb2b1c6
Sercan Yersen <sercan@jotform.com>
2021-07-01 11:33:16 +0300

    fix(auth): set access type and app name on init

--- src/sagas/appSaga.js
+++ src/sagas/appSaga.js
@@ -3,6 +3,7 @@ import { INIT_APP } from '../constants/actionTypes';
 
 function* initApp() {
   // eslint-disable-next-line no-console
+  window.JF.initialize({ accessType: 'full', appName: 'JotForm Polls' });
   console.log('initApp saga called');
   yield;
 }
