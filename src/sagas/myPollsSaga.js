514cf790eaf7e60e4647ccf44ca37db693a54fda
kursadguzelkaya <kursad.gzlky@gmail.com>
2021-07-01 17:58:28 +0300

    fix(myPolls): get submissons of forms

--- src/sagas/myPollsSaga.js
+++ src/sagas/myPollsSaga.js
@@ -7,24 +7,24 @@ import {
   TAKE_USER_POLLS_REQUEST,
   TAKE_USER_POLLS_SUCCESS,
 } from '../constants/actionTypes';
-import { getQuestionsOfForm, getUserForms } from '../lib/api/unsplashService';
+import { getFormSubmissions, getQuestionsOfForm, getUserForms } from '../lib/api/unsplashService';
 
 function* takeUserPolls(action) {
-  console.log('take user polls');
-
   try {
+    // Get API Key
+    const API_KEY = action.payload;
+
     // Get user's forms
-    const { status, data: { content } } = yield call(getUserForms, action.payload);
+    const { status, data: { content } } = yield call(getUserForms, API_KEY);
 
     if (status !== 200) {
       throw Error('Request failed for forms');
     }
-
+    console.log(content);
     // Extract polls from forms
     const polls = [];
     content.map(form => (form.title.substring(0, 10) === '__JFPoll__' ? polls.push(form) : null));
     console.log(polls);
-
     // Get questions for each form
     const newPolls = [];
     for (let i = 0; i < polls.length; i += 1) {
@@ -38,7 +38,19 @@ function* takeUserPolls(action) {
       const pollName = title.substring(11, title.length - 1);
 
       // Get questions
-      const { data } = yield call(getQuestionsOfForm, action.payload, id);
+      const { data } = yield call(getQuestionsOfForm, API_KEY, id);
+      console.log(data);
+
+      // Get submissons
+      const res = yield call(getFormSubmissions, API_KEY, id);
+
+      // Update votes with submissons
+      const options = data.content['3'].options.split('|').map(option => {
+        let votes = 0;
+        // eslint-disable-next-line no-return-assign
+        res.data.content.map(submisson => (submisson.answers['3'].answer === option ? votes += 1 : null));
+        return ({ optionText: option, votes });
+      });
 
       // Create new poll object from form
       const newPoll = I.fromJS({
@@ -49,7 +61,7 @@ function* takeUserPolls(action) {
         status: 'finished', // TODO: Update status
         question: {
           questionText: data.content['3'].text,
-          options: data.content['3'].options.split('|').map(text => ({ optionText: text, votes: 0 })), // TODO: Update votes
+          options,
         },
       });
       newPolls.push(newPoll);
@@ -60,6 +72,7 @@ function* takeUserPolls(action) {
       payload: newPolls,
     });
   } catch (error) {
+    console.log(error);
     yield put({
       type: TAKE_USER_POLLS_FAIL,
       payload: error.message,
