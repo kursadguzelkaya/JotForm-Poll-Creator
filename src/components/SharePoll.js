d155cbedb9596b5d55b71caa56ceda4e686c3ae0
kursadguzelkaya <kursad.gzlky@gmail.com>
2021-07-07 23:36:55 +0300

    feat(share): add copy link functionality

--- src/components/SharePoll.js
+++ src/components/SharePoll.js
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { string } from 'prop-types';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 import '../styles/SharePoll.css';
 
 const SharePoll = ({ pollName, id }) => {
-  console.log('Share');
+  const [copied, setCopied] = useState(false);
   return (
     <div className="share-poll">
       <div className="share-container">
-        <p>{`Poll Name: ${pollName}`}</p>
-        <p>{`Poll Link: http://localhost:3000/poll/${id}`}</p>
-        <button id="copy-btn" type="button" className="btn">
-          <i className="far fa-copy icon" />
-          <span className="none-mobile-600">Copy Link</span>
-        </button>
+        <p className="share">{`Poll Name: ${pollName}`}</p>
+        <p className="share">{`Poll Link: http://localhost:3000/poll/${id}`}</p>
+        <CopyToClipboard
+          text={`Poll Link: http://localhost:3000/poll/${id}`}
+          onCopy={() => setCopied(true)}
+        >
+          <button id="copy-btn" type="button" className="btn">
+            <i className="far fa-copy icon" />
+            <span className="none-mobile-600">Copy Link</span>
+          </button>
+        </CopyToClipboard>
         <button id="whatsapp-btn" type="button" className="btn">
           <i className="fab fa-whatsapp icon" />
           <span className="none-mobile-600">Share on Whatsapp</span>
         </button>
+        {copied ? (
+          <div className="copied-success">
+            <p>Copied successfully</p>
+          </div>
+        ) : null}
       </div>
     </div>
   );
