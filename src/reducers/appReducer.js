8cca0175269a60cbbce99074d240100459547a61
kursadguzelkaya <kursad.gzlky@gmail.com>
2021-07-07 22:48:00 +0300

    fix(socket): fix the bug of reinitilaziton of socket

--- src/reducers/appReducer.js
+++ src/reducers/appReducer.js
@@ -4,12 +4,18 @@ import { INIT_SOCKET } from '../constants/actionTypes';
 const INITIAL_STATE = I.fromJS({
   text: '',
   socket: {},
+  socketStatus: '',
 });
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case INIT_SOCKET: {
-      return state.set('socket', action.payload);
+      console.log(action.payload);
+      if (state.get('socketStatus') !== 'connected' && action.payload) {
+        console.log(state.get('socketStatus'));
+        return state.set('socket', action.payload).set('socketStatus', 'connected');
+      }
+      return state;
     }
     default:
       return state;
