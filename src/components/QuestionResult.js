6feea59886a78c48c477bc5e3043df6050374874
kursadguzelkaya <kursad.gzlky@gmail.com>
2021-07-08 19:13:26 +0300

    fix(options): convert immutuable list to normal list to use map properly

--- src/components/QuestionResult.js
+++ src/components/QuestionResult.js
@@ -5,14 +5,23 @@ import I from 'immutable';
 import '../styles/Result.css';
 import OptionProgress from './OptionProgress';
 
-const QuestionResult = ({ question, totalVotes }) => (
-  <div className="question">
-    <h2>{question.get('questionText')}</h2>
-    <div className="options">
-      {question.get('options').map(option => (<OptionProgress key={option} optionText={option.get('optionText')} optionResult={option.get('votes') !== 0 ? parseInt((option.get('votes') / totalVotes) * 100, 10) : 0} />))}
+const QuestionResult = ({ question, totalVotes }) => {
+  const options = question.get('options').toJS();
+  return (
+    <div className="question">
+      <h2>{question.get('questionText')}</h2>
+      <div className="options">
+        {options.map(option => (
+          <OptionProgress
+            key={option}
+            optionText={option.optionText}
+            optionResult={option.votes !== 0 ? parseInt((option.votes / totalVotes) * 100, 10) : 0}
+          />
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 QuestionResult.propTypes = {
   question: instanceOf(I.Map).isRequired,
