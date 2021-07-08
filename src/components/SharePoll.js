77f826d7e762c2635092d5d496cc08895aa0ac54
kursadguzelkaya <kursad.gzlky@gmail.com>
2021-07-08 14:33:20 +0300

    fix(share): remove CopyToClipBoard

--- src/components/SharePoll.js
+++ src/components/SharePoll.js
@@ -1,25 +1,19 @@
 import React, { useState } from 'react';
 import { string } from 'prop-types';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 import '../styles/SharePoll.css';
 
 const SharePoll = ({ pollName, id }) => {
-  const [copied, setCopied] = useState(false);
+  const [copied] = useState(false);
   return (
     <div className="share-poll">
       <div className="share-container">
         <p className="share">{`Poll Name: ${pollName}`}</p>
         <p className="share">{`Poll Link: http://localhost:3000/poll/${id}`}</p>
-        <CopyToClipboard
-          text={`http://localhost:3000/poll/${id}`}
-          onCopy={() => setCopied(true)}
-        >
-          <button id="copy-btn" type="button" className="btn">
-            <i className="far fa-copy icon" />
-            <span className="none-mobile-600">Copy Link</span>
-          </button>
-        </CopyToClipboard>
+        <button id="copy-btn" type="button" className="btn">
+          <i className="far fa-copy icon" />
+          <span className="none-mobile-600">Copy Link</span>
+        </button>
         <button id="whatsapp-btn" type="button" className="btn">
           <i className="fab fa-whatsapp icon" />
           <span className="none-mobile-600">Share on Whatsapp</span>
