df465e4670b0cc72bfe6abc433f522f72e0be52f
kursadguzelkaya <kursad.gzlky@gmail.com>
2021-07-09 09:03:57 +0300

    fix(optionResult): fix the bug when seen at 100%

--- src/components/OptionProgress.js
+++ src/components/OptionProgress.js
@@ -13,11 +13,11 @@ const OptionProgress = ({ optionText, optionResult }) => {
         <div className="progress-done" data_done={optionResult} style={{ width: `${optionResult}%`, opacity: '1' }}>
           {optionResult > 15 ? `${optionResult}%` : null}
         </div>
-        <span className="percantage">
-          {' '}
-          {optionResult <= 15 ? `${optionResult}%` : null}
-          {' '}
-        </span>
+        {optionResult <= 15 ? (
+          <span className="percantage">
+            { `${optionResult}%` }
+          </span>
+        ) : null}
       </div>
     </div>
   );
