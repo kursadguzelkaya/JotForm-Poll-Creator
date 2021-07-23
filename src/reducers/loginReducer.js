c7a1262c7b6c649c58bcb8d3e11d347d29460ebf
kursadguzelkaya <kursad.gzlky@gmail.com>
2021-07-23 21:16:57 +0300

    fix(API_KEY): save key to local storage and use from there

--- src/reducers/loginReducer.js
+++ src/reducers/loginReducer.js
@@ -3,17 +3,19 @@ import I from 'immutable';
 import { LOG_IN_SUCCESS, LOG_OUT } from '../constants/actionTypes';
 
 const INITIAL_STATE = I.fromJS({
-  status: 'not-authenticated',
-  key: '',
+  status: localStorage.getItem('key') ? 'authenticated' : 'not-authenticated',
+  key: localStorage.getItem('key'),
 });
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case LOG_IN_SUCCESS: {
+      localStorage.setItem('key', action.payload);
       return state.set('status', 'authenticated').set('key', action.payload);
     }
     case LOG_OUT: {
-      return state.set('status', 'not-authenticated');
+      localStorage.removeItem('key');
+      return state.set('status', 'not-authenticated').set('key', null);
     }
     default:
       return state;
